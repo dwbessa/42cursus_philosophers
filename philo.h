d3fca0452f4667b62f4b17962ae104c4f2e21628
dwbessa <danielbessa01@gmail.com>
2024-05-23 21:16:27 -0300

    first commit scratching things

---
+++ philo.h
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/23 20:48:12 by dbessa            #+#    #+#             */
+/*   Updated: 2024/05/23 21:09:23 by dbessa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <stdbool.h>
+# include <limits.h>
+
+//YouTube Oceano
+
+# define RED     "\x1b[31m"
+# define GREEN   "\x1b[32m"
+# define YELLOW  "\x1b[33m"
+# define CYAN    "\x1b[36m"
+# define RST   "\x1b[0m"
+
+typedef pthread_mutex_t t_mtx;
+typedef	struct s_table t_table;
+
+typedef struct s_fork
+{
+	t_mtx	fork;
+	int		fork_id;
+}				t_fork;
+
+typedef struct s_philo
+{
+	int			id;
+	long		diner_counter;
+	bool		full;
+	long		last_meal_time;
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+	pthread_t	thread_id;
+	t_table		*table;
+}				t_philo;
+
+struct s_table
+{
+	long	philo_nbr;
+	long	time_to_die;
+	long	time_to_eat;
+	long	time_to_sleep;
+	long	nbr_limit_meals;
+	long	start_simulation;
+	bool	end_simulation;
+	t_fork	*forks;
+	t_philo	*philos;
+}				t_table;
+
+void	error_exit(const char *error_msg);
+#endif
